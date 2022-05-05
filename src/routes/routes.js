3b518e2c2f5f6fb409fe2052ee0ddc3c400f19bf
komildeveloper <komildeveloper111@gmail.com>
2022-05-05 09:53:41 +0500

    updates

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,9 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.json({
-    msg: 'Hello Express'
-  })
+  res.render('index')
 })
 
 module.exports = {
---
+++ src/server.js
@@ -0,0 +1,21 @@
+require('dotenv').config({ path: './src/.env' })
+const express = require('express');
+const { router } = require('./routes/routes')
+
+const app = express();
+
+app.use(express.json())
+app.set('views', join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.use('/static', express.static(join(__dirname, 'public')))
+
+app.get('/', router)
+
+app.get('/*', (_, res) => {
+  res.sendStatus(404)
+})
+
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
